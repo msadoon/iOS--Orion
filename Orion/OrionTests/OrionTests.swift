419047942ad5a250ade50809269747b1898349de
Mubarak Sadoon <msadoon@gmail.com>
2024-12-07 11:25:43 -0500

    new uikit project

---
+++ Orion/OrionTests/OrionTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrionTests.swift
+//  OrionTests
+//
+//  Created by Mubarak Sadoon on 2024-12-07.
+//
+
+import Testing
+@testable import Orion
+
+struct OrionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
