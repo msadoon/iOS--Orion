71b9123919a86a7025c81cd213dc5901a8a69e73
Mubarak Sadoon <msadoon@gmail.com>
2024-12-08 18:52:06 -0500

    adjusting webview padding to be flush with page control at bottom and not overlap notch.

--- Orion/Orion/AppDelegate.swift
+++ Orion/Orion/AppDelegate.swift
@@ -2,9 +2,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //FIXME: Remove later - including on the paging control.
